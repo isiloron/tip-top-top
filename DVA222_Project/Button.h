0f7e71850d0631bffdd6277dda920a1e901e053a
linus <isiloron@gmail.com>
2015-05-27 15:46:51 +0200

    added filters

--- DVA222_Project/Button.h
+++ DVA222_Project/Button.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "stdafx.h"
+#include "Graphix.h"
+#include "Label.h"
+
+class Button : public ControlBase
+{
+private:
+	Bitmap *normal, *hover, *press;
+	Label *label;
+public:
+	Button();
+	Button(int locX, int locY, int width, int height);
+	~Button();
+
+
+
+};
